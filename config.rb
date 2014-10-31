0a54b187373de8016092cb4dd877c305bdea1d79
Andrea Kennedy <asrkennedy@gmail.com>
2014-10-31 05:18:13 +0000

    converts all to sass, brings in foundation and makes index mobile responsive

--- config.rb
+++ config.rb
@@ -3,12 +3,16 @@
 require 'susy'
 require 'breakpoint'
 require 'rgbapng'
+
+# Path to foundation files
+add_import_path "bower_components/foundation/scss"
+
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-sass_dir = "stylesheets/sass"
-css_dir = "stylesheets/css"
+css_dir = "stylesheets"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 line_comments = false
--- js/main.js
+++ js/main.js
@@ -1,56 +1,81 @@
+var aboveTablet = window.matchMedia("only screen and (min-width: 1024px)");
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(40.740829, -74.173124);
+  var mapOptions = {
+    zoom: 16,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+function animateScroll(clickedLink){
+   var target = "#" + $(clickedLink).data('target');
+    $('html, body').animate({
+        scrollTop: ($(target).offset().top)- 51
+    }, 1000);
+}
+
 $(document).ready(function(){
-
-	function initialize() {
-	  var myLatlng = new google.maps.LatLng(40.740829, -74.173124);
-	  var mapOptions = {
-	    zoom: 16,
-	    center: myLatlng
-	  }
-	  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-
-	  var marker = new google.maps.Marker({
-	      position: myLatlng,
-	      map: map,
-	      title: 'Hello World!'
-	  });
-	}
 
 	google.maps.event.addDomListener(window, 'load', initialize);
 
-	function animateScroll(clickedLink){
-     var target = "#" + $(clickedLink).data('target');
-      $('html, body').animate({
-          scrollTop: ($(target).offset().top)- 51
-      }, 1000);
-  }
-
-   $( '#index nav ul li a' ).on('click', function(event) {
-      event.preventDefault();
-      $('.active').removeClass();
-      $(this).addClass('active');
-      animateScroll(this);
+  $( '#index nav ul li a' ).on('click', function(event) {
+    event.preventDefault();
+    $('.active').removeClass();
+    $(this).addClass('active');
+    animateScroll(this);
   });
 
-   $( 'aside ul li a, .more-training' ).on('click', function(e){
-   	e.preventDefault();
-   	$('.clicked').removeClass();
-   	$(this).addClass('clicked');
-   	animateScroll(this);
-   })
+  $( 'aside ul li a, .more-training' ).on('click', function(e){
+  	e.preventDefault();
+  	$('.clicked').removeClass();
+  	$(this).addClass('clicked');
+  	animateScroll(this);
+  });
 
-   $('.download').on('click', function(){
-   		animateScroll(this);
-   })
-   		
-   $('.rtm-users').hover(function(){
-   		$(this).css('width','20%');
-   },function(){
-   		$(this).css('width','10%');
-   })
+  $('.download').on('click', function(){
+  		animateScroll(this);
+  });
 
-   $('.contact-right').click(function () {
+  //Animate police logos above tablet
+  if (aboveTablet.matches) {
+    $('.rtm-users').hover(function(){
+    		$(this).css('width','20%');
+    },function(){
+    		$(this).css('width','10%');
+    });
+    $('#mobile-menu').hide();
+    $('nav ul li').width('13%');
+    $('nav').removeClass('row');
+  } else {
+    //show mobile menu
+    $('nav').addClass('row');
+    $('#mobile-menu').show();
+    $('nav ul').hide();
+  }
+
+  $('#mobile-menu').on('click', function(){
+    $('nav ul').toggle(function(){
+      $('this').slideDown();
+    },function(){
+      $('this').slideUp();
+    });
+  });
+
+
+  $('.contact-right').click(function () {
     $('#map_canvas').css("pointer-events", "auto");
-});
+
+  });
+
+
 
 
 })
